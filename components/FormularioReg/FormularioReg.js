92f7d62049aca3076b73b9dab606a5e2aa9f1054
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2022-10-08 20:47:58 -0500

    axios-search-filter

--- components/FormularioReg/FormularioReg.js
+++ components/FormularioReg/FormularioReg.js
@@ -2,6 +2,7 @@
 import logo from "./Hotelia horizontal blanco.svg";
 import axios from "axios";
 import { useState } from "react";
+import Swal from 'sweetalert2';
 //import { useHistory } from "react-router";
 
 import "./FormularioReg.css";
@@ -37,18 +38,20 @@ function FormualrioRegis({ TituloRegister }) {
         console.log(response);
         if (response.status === 201) {
 
-            alert(
+            Swal.fire(
                 'Guardado!'
-                //    `El estudiante <strong> ${response.data.nombre} ${response.data.apellido}</strong> ha sido guardado exitosamente!`,
-                //    'success'
+                   `El estudiante <strong> ${response.data.nombre} ${response.data.apellido}</strong> ha sido guardado exitosamente!`,
+                   'success'
             )
             //    history.push("/");
 
         } else {
-            alert(
+            Swal.fire(
                 'Guardado!',
-                'Hubo un problema al registrar el estudiante!',
-                'error'
+                `El Usuario ha sido guardado exitosamente!`,
+                'success'
+                // 'Hubo un problema al registrar el estudiante!',
+                // 'error'
             )
         }
     }
@@ -65,104 +68,7 @@ function FormualrioRegis({ TituloRegister }) {
                     <h1>Registrate</h1>
                 </div>
 
-                {/* <form action="" method="post">
-                    <div class="formulario">
-                        <div class="columna">
-                            <div>
-                                <label for="tipo">Tipo de documento</label>
-                                <select name="select">
-                                    <option selected>Seleccione una opcion</option>
-                                    <option>Tarjeta de identidad</option>
-                                    <option>Cedula de ciudadania</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label for="nombre">Nombre</label>
-                                <input type="text" name="nombre" id="nombre" />
-                            </div>
-                            <div>
-                                <label for="fecha">Fecha de nacimiento</label>
-                                <input type="date" name="fecha" />
-                            </div>
-
-                            <div>
-                                <label for="email">Email</label>
-                                <input type="email" placeholder="e-mail" />
-                            </div>
-                            <div>
-                                <label for="pais">Pais de origen</label>
-                                <input type="text" name="pais" id="pais" />
-                            </div>
-                            <div>
-                                <label>Contraseña</label>
-                                <input type="password" />
-                            </div>
-                        </div>
-                        <div class="columna">
-                            <div>
-                                <label for="doc">Numero de documento</label>
-                                <input type="number" name="doc" id="doc" />
-                            </div>
-                            <div>
-                                <label for="apellido">Apellido</label>
-                                <input type="text" name="apellido" id="apellido" />
-                            </div>
-                            <div>
-                                <label>Genero</label>
-                                <div class="genero">
-                                    <div className="gen"><input type="radio" /><label>Mujer</label></div>
-                                    <div className="gen"><input type="radio" /><label>Hombre</label></div>
-                                    <div className="gen"><input type="radio" /><label>Otro</label></div>
-                                </div>
-                            </div>
-                            <div>
-                                <label for="telefono">Telefono</label>
-                                <input type="number" name="telefono" id="telefono" />
-                            </div>
-                            <div class="foto">
-                                <label>Foto</label>
-                                <input type="file" id="archivo" /> 
-                                <label for="archivo" id="archivo">Selecionar Archivo</label>
-                            </div>
-                            <div>
-                                <label>Confirmar contraseña</label>
-                                <input type="password" />
-                            </div>
-                        </div>
-                    </div>
-
-                    <input type="checkbox" id="btn-modal" />
-                    <label for="btn-modal" class="lbl-modal">Terminos y condiciones</label>
-
-                    <div class="boton">
-                        <Link to='/login'> Ingresar</Link>
-                    </div>
-
-
-                    <div class="modal">
-                        <div class="contenedor">
-                            <header>Terminos y Condiciones</header>
-                            <label for="btn-modal">X</label>
-                            <div class="contenido">
-
-                                <p>
-                                    Estos Términos del Servicio reflejan la forma de trabajar de Google como empresa, las leyes por las que nos regimos y
-                                    determinados aspectos que siempre hemos creido que son ciertos. Por ello, estos Términos del Servicio ayudan a definir
-                                    la relación que tiene Google contigo cuando interactúas con nuestros servicios, Por ejemplo, estos términos incluyen las
-                                    siguientes secciones:
-                                </p>
-                                <p>
-                                    Qué puedes esperar de nosotros, donde se describe como proporcionamos y desarrollamos nuestros servicios. Lo que
-                                    esperamos de ti, donde se establecen ciertas reglas para utilizar nuestros servicios.
-                                </p>
-
-                                <button>Aceptar</button>
-                            </div>
-
-
-                        </div>
-                    </div>
-                </form> */}
+              
 
                 <form onSubmit={handleSubmit} action="" method="post">
                     <div class="formulario">
--- components/TableUsers/TableUsers.js
+++ components/TableUsers/TableUsers.js
@@ -10,68 +10,95 @@ function TableUsers() {
     const url = "https://hoteliakuepa.herokuapp.com/users";
 
     const [list, setList] = useState([]);
+    const [search, setSearch] = useState("")
 
     const getData = async () => {
         const response = axios.get(url);
         return response;
     }
 
+    //función de búsqueda
+    const searcher = (e) => {
+        setSearch(e.target.value)
+        // console.log(e.target.value)
+    }
+
+    // metodo de filtrado 1 
+
+    let results = []
+    if (!search) {
+        results = list
+    } else {
+        results = list.filter((dato) =>
+            dato.nombre.toLowerCase().includes(search.toLocaleLowerCase())
+        )
+    }
+
+    // metodo de filtrado 2
+
+    // const results = !search ? list : list.filter((dato)=> dato.nombre.toLowerCase().includes(search.toLocaleLowerCase()))
+
+
     useEffect(() => {
         getData().then((response) => {
             setList(response.data);
-            console.log(response.data);
+            // console.log(response.data);
         })
     }, []);
+
 
 
     return (
 
         <body className="fondo-dashboard">
 
-        <div id="container-table-users">
+            <input value={search} onChange={searcher} type="text" placeholder='Search' />
 
-            <table className="table-users">
-                <thead className="thead-users">
-                    <tr>
-                        <th className="th-users">ID</th>
-                        <th className="th-users">Tipo Doc</th>
-                        <th className="th-users">Nombre</th>
-                        <th className="th-users">Apellido</th>
-                        <th className="th-users">Fecha nacimiento</th>
-                        <th className="th-users">Genero</th>
-                        <th className="th-users">Email</th>
-                        <th className="th-users">telefono</th>
-                        <th className="th-users">Pais</th>
-                        <th className="th-users">password</th>
-                        <th className="th-users">Rol</th>
-                        <th className="th-users">img</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    {list.map((list, index) => (
+            <div id="container-table-users">
+
+                <table className="table-users">
+                    <thead className="thead-users">
                         <tr>
-                            <td className="td-users">{list._id}</td>
-                            <td className="td-users">{list.tipodoc}</td>
-                            <td className="td-users">{list.nombre}</td>
-                            <td className="td-users">{list.apellido}</td>
-                            <td className="td-users">{list.fnacimiento}</td>
-                            <td className="td-users">{list.genero}</td>
-                            <td className="td-users">{list.email}</td>
-                            <td className="td-users">{list.telefono}</td>
-                            <td className="td-users">{list.paisorigen}</td>
-                            <td className="td-users">{list.password}</td>
-                            <td className="td-users">{list.tipouser}</td>
-                            <td className="td-users">{list.img}</td>
-                           
+                            <th className="th-users">ID</th>
+                            <th className="th-users">Tipo Doc</th>
+                            <th className="th-users">Nombre</th>
+                            <th className="th-users">Apellido</th>
+                            <th className="th-users">Fecha nacimiento</th>
+                            <th className="th-users">Genero</th>
+                            <th className="th-users">Email</th>
+                            <th className="th-users">telefono</th>
+                            <th className="th-users">Pais</th>
+                            <th className="th-users">password</th>
+                            <th className="th-users">Rol</th>
+                            <th className="th-users">img</th>
+
                         </tr>
-                    ))}
-                </tbody>
+                    </thead>
+                    <tbody>
+                        {results.map((list, index) => (
+                            <tr>
+                                <td className="td-users">{list._id}</td>
+                                <td className="td-users">{list.tipodoc}</td>
+                                <td className="td-users">{list.nombre}</td>
+                                <td className="td-users">{list.apellido}</td>
+                                <td className="td-users">{list.fnacimiento}</td>
+                                <td className="td-users">{list.genero}</td>
+                                <td className="td-users">{list.email}</td>
+                                <td className="td-users">{list.telefono}</td>
+                                <td className="td-users">{list.paisorigen}</td>
+                                <td className="td-users">{list.password}</td>
+                                <td className="td-users">{list.tipouser}</td>
+                                <td className="td-users">{list.img}</td>
 
-            </table>
-        </div>
+                            </tr>
+                        ))}
+                    </tbody>
 
-                    </body>
+                </table>
+            </div>
+
+        </body>
+
     );
 }
 
